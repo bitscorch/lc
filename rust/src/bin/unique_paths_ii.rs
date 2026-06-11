e8da7c6d8c39270d9009bd3d0a8a0f51e3418686
bitscorch <paul@bitscorch.com>
2026-06-12 02:19:30 +0300

    improve unique-paths-ii mem O(m * n) => O(n)

--- rust/src/bin/unique_paths_ii.rs
+++ rust/src/bin/unique_paths_ii.rs
@@ -63,24 +63,20 @@ struct Solution;
 impl Solution {
     pub fn unique_paths_with_obstacles(obstacle_grid: Vec<Vec<i32>>) -> i32 {
         let (m, n) = (obstacle_grid.len(), obstacle_grid[0].len());
-        let mut dp = vec![vec![0; n]; m];
+        let mut dp = vec![0; n];
+        dp[0] = 1;
 
         for i in 0..m {
             for j in 0..n {
                 if obstacle_grid[i][j] == 1 {
-                    continue;
-                }
-
-                dp[i][j] = match (i, j) {
-                    (0, 0) => 1,
-                    (0, _) => dp[i][j - 1],
-                    (_, 0) => dp[i - 1][j],
-                    _ => dp[i][j - 1] + dp[i - 1][j],
+                    dp[j] = 0;
+                } else if j > 0 {
+                    dp[j] += dp[j - 1];
                 }
             }
         }
 
-        dp[m - 1][n - 1]
+        dp[n - 1]
     }
 }
 
