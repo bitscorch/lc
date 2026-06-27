3610da055abeab615309ff505c946f54e00370ec
bitscorch <paul@bitscorch.com>
2026-06-27 23:42:30 +0300

    os k-closest-points-to-origin rust

--- rust/src/bin/k_closest_points_to_origin.rs
+++ rust/src/bin/k_closest_points_to_origin.rs
@@ -46,8 +46,9 @@ struct Solution;
 
 impl Solution {
     pub fn k_closest(mut points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
-        points.sort_by_key(|p| p[0] * p[0] + p[1] * p[1]);
-        points.truncate(k as usize);
+        let k = k as usize;
+        points.select_nth_unstable_by_key(k - 1, |p| p[0] * p[0] + p[1] * p[1]);
+        points.truncate(k);
         points
     }
 }
