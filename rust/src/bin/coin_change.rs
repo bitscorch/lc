ffa9fcb1bb9015f8d05d50f23168f4115cfdbfa6
bitscorch <paul@bitscorch.com>
2026-07-09 13:45:47 +0300

    s coin-change rust

---
+++ rust/src/bin/coin_change.rs
@@ -0,0 +1,98 @@
+//! 322. Coin Change
+//! Medium | Array | Dynamic Programming | Breadth-First Search
+//! https://leetcode.com/problems/coin-change/
+//!
+//! You are given an integer array `coins` representing coins of different
+//! denominations and an integer `amount` representing a total amount of money.
+//!
+//! Return *the fewest number of coins that you need to make up that amount*. If
+//! that amount of money cannot be made up by any combination of the coins,
+//! return `-1`.
+//!
+//! You may assume that you have an infinite number of each kind of coin.
+//!
+//! **Example 1:**
+//!
+//! ```
+//! Input: coins = [1,2,5], amount = 11
+//! Output: 3
+//! Explanation: 11 = 5 + 5 + 1
+//!
+//! ```
+//!
+//! **Example 2:**
+//!
+//! ```
+//! Input: coins = [2], amount = 3
+//! Output: -1
+//!
+//! ```
+//!
+//! **Example 3:**
+//!
+//! ```
+//! Input: coins = [1], amount = 0
+//! Output: 0
+//!
+//! ```
+//!
+//! **Constraints:**
+//!
+//! * `1 <= coins.length <= 12`
+//! * `1 <= coins[i] <= 2<sup>31</sup> - 1`
+//! * `0 <= amount <= 10<sup>4</sup>`
+
+struct Solution;
+
+impl Solution {
+    pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
+        if amount == 0 {
+            return 0;
+        }
+        let mut dp = vec![i32::MAX; amount as usize + 1];
+
+        dp[0] = 0;
+        for &coin in &coins {
+            if coin > amount {
+                continue;
+            }
+            dp[coin as usize] = 1;
+        }
+
+        for i in 1..=amount as usize {
+            let min = coins
+                .iter()
+                .filter(|c| i as i32 - **c > 0)
+                .map(|c| dp[i - *c as usize])
+                .min();
+
+            match min {
+                Some(i32::MAX) | None => continue,
+                _ => {}
+            }
+            dp[i] = dp[i].min(1 + min.unwrap());
+        }
+
+        if dp[amount as usize] < i32::MAX {
+            dp[amount as usize]
+        } else {
+            -1
+        }
+    }
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(vec![1,2,5], 11, 3)]
+    #[case(vec![2], 3, -1)]
+    #[case(vec![1], 0, 0)]
+    fn cases(#[case] coins: Vec<i32>, #[case] amount: i32, #[case] expected: i32) {
+        assert_eq!(expected, Solution::coin_change(coins, amount));
+    }
+}
